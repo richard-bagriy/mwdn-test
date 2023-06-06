b98a3742fabcdb958192bb9ab1b61adf05f52f4b
Richard Bagriy <richard.bagriy@gmail.com>
2023-06-06 18:57:11 +0300

    added cors to be

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api');
   const config = app.get(ConfigService);
   await app.listen(config.get('app.post'));
